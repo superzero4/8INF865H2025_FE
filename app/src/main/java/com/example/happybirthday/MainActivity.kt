cc9823d48a0f1a080d613676417fb0f90123aac9
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-21 22:18:30 -0500

    End of step 5
    (Fixes center issue in last exercise)

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +50,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box(modifier,) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message,
@@ -74,8 +77,7 @@ fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
             Text(
                 text = from,
                 fontSize = 36.sp,
-                textAlign = TextAlign.End,
-                modifier = modifier.padding(16.dp)
+                modifier = Modifier.padding(16.dp).align(Alignment.End)
             )
     }
 }
