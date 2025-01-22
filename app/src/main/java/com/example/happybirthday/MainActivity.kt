0450f19e082de9dc5f92ba1fb29d696a4573c0d1
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-21 21:33:59 -0500

    End of step 7

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -29,11 +29,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp
     )
 }
 
@@ -41,6 +45,6 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Florentin!")
+        GreetingText(message = "Happy Birthday Florentin!", from = "From Gégé")
     }
 }
