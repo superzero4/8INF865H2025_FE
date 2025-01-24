a8530645382db0da3e21ea70ebbccc3e30df8c5d
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-23 22:53:54 -0500

    End of step 8

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -32,7 +33,10 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -46,6 +50,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Switch
 import androidx.compose.ui.text.input.ImeAction
@@ -85,7 +91,8 @@ fun TipTimeLayout() {
         modifier = Modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
-            .safeDrawingPadding(),
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -102,11 +109,12 @@ fun TipTimeLayout() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next,
             ),
+            leadingIcon = R.drawable.money,
             value = amountInput,
             onValueChange = {
                 amountInput = it
             },
-            Modifier
+            modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
@@ -115,6 +123,7 @@ fun TipTimeLayout() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Done,
             ),
+            leadingIcon = R.drawable.percent,
             label = R.string.how_was_the_service,
             value = tipInput,
             onValueChange = {
@@ -159,6 +168,7 @@ fun RoundTheTipRow(modifier: Modifier = Modifier,
 @Composable
 private fun EditNumberField(
     @StringRes label: Int,
+    @DrawableRes leadingIcon: Int,
     keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
@@ -171,7 +181,8 @@ private fun EditNumberField(
         singleLine = true,
         keyboardOptions = keyboardOptions,
         onValueChange = onValueChange,
-        label = { Text(stringResource(label)) }
+        label = { Text(stringResource(label)) },
+        leadingIcon = { Icon(painter = painterResource(leadingIcon),null)}
     )
 }
 
